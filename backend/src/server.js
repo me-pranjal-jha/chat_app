b4416f85e1f217c3c114566dd8352bd997a5e89e
Cranial Belgrade <prnjljh@gmail.com>
2026-03-14 01:18:54 +0530

    ready for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,24 +1,26 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import authRoutes from './routes/auth.routes.js';
-import messageRoutes from './routes/message.routes.js';
-import path from 'path';
+import express from "express";
+import dotenv from "dotenv";
+import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
+import path from "path";
+
 dotenv.config();
 
 const app = express();
+const __dirname = path.resolve();
+const PORT = process.env.PORT || 3000;
 
-app.use('/api/auth', authRoutes);
-app.use('/api/messages', messageRoutes);
+app.use(express.json());
 
-//MAKE READY FOR DEPLOYMENT
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
-  });
-}
+app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server is running on port ${process.env.PORT}`);
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
