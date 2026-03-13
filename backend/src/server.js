52645cf8748ca8841f9698361685d61ffc08afb4
Cranial Belgrade <prnjljh@gmail.com>
2026-03-13 23:46:41 +0530

    initial commit

---
+++ backend/src/server.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import authRoutes from './routes/auth.routes.js';
+import messageRoutes from './routes/message.routes.js';
+import path from 'path';
+dotenv.config();
+
+const app = express();
+
+app.use('/api/auth', authRoutes);
+app.use('/api/messages', messageRoutes);
+
+//MAKE READY FOR DEPLOYMENT
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+  });
+}
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
+});
