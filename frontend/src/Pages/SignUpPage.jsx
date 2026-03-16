1e66b99d162923b0690f1d9ea3ecaca38b70b18a
Cranial Belgrade <prnjljh@gmail.com>
2026-03-17 04:33:43 +0530

    frontend setup done  and bit checks in backend

---
+++ frontend/src/Pages/SignUpPage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function SignUpPage() {
+  return (
+   <div className="h-screen w-full flex items-center justify-center">
+      <h1 className="text-white text-5xl font-bold drop-shadow-lg">
+        SignUpPage
+      </h1>
+    </div>
+  );
+}
+
+export default SignUpPage;
