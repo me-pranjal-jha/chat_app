8c58ad920ad59d1c01099b975ef78bf0c14345a7
Cranial Belgrade <prnjljh@gmail.com>
2026-03-17 23:29:43 +0530

    signup implemented on the frontend

--- backend/src/controllers/auth.controllers.js
+++ backend/src/controllers/auth.controllers.js
@@ -16,8 +16,8 @@ export const signup = async (req , res) =>{
         return res.status(400).json({ message: 'All fields are required' });
     }
 
-    if(password.length < 6){
-        return res.status(400).json({ message: 'Password must be at least 6 characters long' });
+    if(password.length < 8){
+        return res.status(400).json({ message: 'Password must be at least 8 characters long' });
     }
 
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
